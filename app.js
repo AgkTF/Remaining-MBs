dd8c0ac016ed6ed6010e71733897e11bc9c73a38
agkz1st <ahmed.agk22@gmail.com>
2019-08-31 15:45:06 +0200

    Navigate to sms page successfully.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const screenshot = 'router-login.png';
+const screenshot = 'remaining-MBs.png';
 (async () => {
 	const url = 'http://192.168.0.1/login.asp';
 	try {
@@ -9,12 +9,27 @@ const screenshot = 'router-login.png';
 		await page.goto(url);
 		await page.type('#ID', 'admin');
 		await page.type('#PASSWORD', 'thp2safer');
+
 		await Promise.all([
 			page.waitForNavigation(),
 			page.click('#wan_change_mode')
 		]);
+
+		await Promise.all([page.waitForNavigation(), page.click('#navi-sms')]);
+
+		await page.waitForSelector('iframe');
+		console.log('iframe is ready. Loading iframe content');
+
+		const elementHandle = await page.$('iframe[id="interFrame"]');
+
+		const frame = await elementHandle.contentFrame();
+		await frame.click('a[href="#tabs-2"]');
+		await frame.type('#ussd_cmd', '');
+
 		await page.screenshot({ path: screenshot, fullPage: true });
+
 		browser.close();
+
 		console.log('See screenshot: ' + screenshot);
 	} catch (error) {
 		console.log(error);
